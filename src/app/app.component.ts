8781a37b21e1a319dc767fa38a66fd46cd99ffab
Entropy16 <s.narvaez@utp.edu.co>
2021-12-06 12:02:27 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './model/Usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Proyecto';
+
+  public nombres: string[] = ["Diego", "Sebas"];
+
+  public nombres2: string[] = ["Andres"];
+
+  public listUsuarios: Usuario[] = [];
+
+  constructor(){
+    this.addFirstElement("Jhon");
+    this.addLastElement("Jorge");
+    this.deletelastElement()
+    console.log(this.nombres);
+
+    let usuario:Usuario = new Usuario();
+    usuario.idUsuario = 1;
+    usuario.edad = 20;
+    usuario.nombre = "Ricardo";
+    usuario.estado = true;
+    
+    let usuario2:Usuario = new Usuario();
+    usuario2.idUsuario = 2;
+    usuario2.edad = 23;
+    usuario2.nombre = "José"
+    usuario2.estado = true;
+
+    this.listUsuarios.push(usuario)
+    this.listUsuarios.push(usuario2)
+  }
+
+  public addFirstElement(nombre: string): void{
+    this.nombres.unshift(nombre);
+  }
+
+  public addLastElement(nombre: string): void{
+    this.nombres.push(nombre);
+  }
+
+  public deletelastElement(): void{
+    let res = this.nombres.pop();
+    console.log("Se eliminó" + res)
+  }
+
+  public contacList(list:string[]): string[]{
+    return this.nombres.concat(list);
+  }
+
 }
+
+
---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,6 @@
+export class Usuario{
+    public idUsuario: number = 0;
+    public nombre: string = "";
+    public edad: number = 0;
+    public estado: boolean = false;
+}
